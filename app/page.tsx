24ace7ab92b61b475e85e4422c5845ae9c8b7148
linggarelangp <linggar382@gmail.com>
2025-05-09 15:04:00 +0700

    jokopi-app

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react';
+
+const Home = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Welcome to Jokopi!</h1>
+      <p>This is a Homepage</p>
+    </div>
+  );
+}
+export default Home;
